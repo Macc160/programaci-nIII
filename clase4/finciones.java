7130a804c0bc8fe0488a681dd1f83242576145bf
Macc160 <madolfocortes@estudiante.uniajc.edu.co>
2025-09-02 05:09:57 +0000

    como declarar una funcion

---
+++ clase4/finciones.java
@@ -0,0 +1,17 @@
+public class finciones {
+    
+    public static void main(String[] args) {
+
+        // codigo
+        CalculaAreaRectangulo();
+
+    }
+    public static void CalculaAreaRectangulo(){
+        double base = 10;
+        double altura = 5;
+        double areaRectangulo = 0;
+
+        areaRectangulo = (base * altura)/2;
+        System.out.println(" el area del rectangulo es:"+ areaRectangulo);
+    }
+}
