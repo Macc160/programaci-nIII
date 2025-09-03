ca744597c7c81e6e3fd7d7c1b07648295da2c3a7
Macc160 <madolfocortes@estudiante.uniajc.edu.co>
2025-09-03 01:13:38 +0000

    prc

---
+++ clase4/Condicionales.java
@@ -0,0 +1,26 @@
+public class Condicionales{
+
+public static void main(String[] args) {
+
+int hora = 10;
+// 1 hora a 11 horas
+if (hora>=1 && hora<=11 ){
+System.out.println("buenas dias");
+
+// 12 horas a 17 horas
+}else if (hora>= 12 && hora<=17) {
+    System.out.println("buenas tardes");
+}
+// 18 horas a 24 horas
+ if (hora>= 18 && hora<=24){
+    System.out.println("buenas noches");
+} else{
+    System.out.println("esto no exite");
+} 
+}
+
+
+}
+
+
+
