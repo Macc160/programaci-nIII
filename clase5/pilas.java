0bdff7ea34dcabae06e9a52401c46d45e4c3d41c
Macc160 <madolfocortes@estudiante.uniajc.edu.co>
2025-09-16 17:01:45 +0000

    pilas

---
+++ clase5/pilas.java
@@ -0,0 +1,23 @@
+public class OperaciboolenaonesPila{
+    public boolean validarParentesis(String[] s){
+        boolean flag = false 
+        pilaSignos = new Stack <>();
+        
+        for (int 1 = 0; i < s.leangth; i++){
+        pilaSignos.push(s[i]);
+     }
+    int tam = pilasSignos.size();
+    // recorrer la pila
+    for ( int 1 = 0; i < tam; i++ ){
+        if (pilasSignos.peek().equals("("))){
+
+            cont1 += 1,
+            else{
+                cont2 += 1;
+                flag = (cont1 == cont2 ? true : false);
+            }
+        }
+    }
+
+    }
+}
